2b0e808c616d5bdae5530d13bcfbd05fdb36e229
artypeace <1arty4u@gmail.com>
2023-06-01 16:18:53 +0700

    Initial Commit

---
+++ SwiftUI-Onboarding-Animation-PayWall-RevenueCat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Onboarding-Animation-PayWall-RevenueCat
+//
+//  Created by AV on 6/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Onboarding-Animation-PayWall-RevenueCat/SwiftUI_Onboarding_Animation_PayWall_RevenueCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Onboarding_Animation_PayWall_RevenueCatApp.swift
+//  SwiftUI-Onboarding-Animation-PayWall-RevenueCat
+//
+//  Created by AV on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Onboarding_Animation_PayWall_RevenueCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
